9805e79ae1100cf6727065f29a1a04a0f484e936
ACH90 <chassagne.alexandre@gmail.com>
2025-05-21 15:51:15 +0200

    Add CSS EmployeeList

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import EmployeeTable from "../../components/EmployeeTable/EmployeeTable";
 import { Link } from "react-router-dom";
+import styles from "./EmployeeList.module.css";
 
 function EmployeeList() {
   const employees = useSelector((state) => state.employee.employees);
 
   return (
     <main style={{ padding: "2rem" }}>
-      <h1>Current Employees</h1>
+      <div className={styles.header}>
+        <h1>Current Employees</h1>
+        <Link to="/">Home</Link>
+      </div>
       <EmployeeTable employees={employees} />
-      <Link to="/">Home</Link>
     </main>
   );
 }
