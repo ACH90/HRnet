57001e1b966bdf6a86b4353e2d1e9d2f85f9d40e
ACH90 <chassagne.alexandre@gmail.com>
2025-05-22 14:01:12 +0200

    Add SearchBar to EmployeeTable

--- src/components/EmployeeTable/EmployeeTable.jsx
+++ src/components/EmployeeTable/EmployeeTable.jsx
@@ -1,33 +1,52 @@
+// components/EmployeeTable.jsx
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { Box, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const columns = [
-  { field: "firstName", headerName: "First Name", flex: 1 },
-  { field: "lastName", headerName: "Last Name", flex: 1 },
-  { field: "startDate", headerName: "Start Date", flex: 1 },
-  { field: "department", headerName: "Department", flex: 1 },
-  { field: "dateOfBirth", headerName: "Date of Birth", flex: 1 },
-  { field: "street", headerName: "Street", flex: 1 },
-  { field: "city", headerName: "City", flex: 1 },
-  { field: "state", headerName: "State", flex: 1 },
-  { field: "zipCode", headerName: "Zip Code", flex: 1 },
+  { field: "id", headerName: "ID", width: 90 },
+  { field: "firstName", headerName: "First name", width: 150 },
+  { field: "lastName", headerName: "Last name", width: 150 },
+  { field: "startDate", headerName: "Start Date", width: 150 },
+  { field: "department", headerName: "Department", width: 150 },
+  { field: "dateOfBirth", headerName: "Date of Birth", width: 150 },
+  { field: "street", headerName: "Street", width: 150 },
+  { field: "city", headerName: "City", width: 100 },
+  { field: "state", headerName: "State", width: 100 },
+  { field: "zipCode", headerName: "Zip Code", width: 100 },
 ];
 
-export default function EmployeeTable({ employees }) {
-  const rows = employees.map((employee, index) => ({
-    id: index, // requis par DataGrid
-    ...employee,
-  }));
+export default function EmployeeTable() {
+  const employees = useSelector((state) => state.employee.employees);
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value.toLowerCase());
+  };
 
+  // Filtrage global sur toutes les valeurs d'un employé
+  const filteredRows = employees.filter((emp) =>
+    Object.values(emp).some((val) =>
+      String(val).toLowerCase().includes(searchText)
+    )
+  );
   return (
     <div style={{ height: 600, width: "100%" }}>
+      <TextField
+        label="Rechercher un employé"
+        variant="outlined"
+        // fullWidth
+        margin="normal"
+        onChange={handleSearchChange}
+      />
       <DataGrid
-        rows={rows}
+        rows={filteredRows.map((emp, index) => ({ id: index, ...emp }))}
         columns={columns}
         pageSize={10}
         rowsPerPageOptions={[10, 25, 50]}
-        autoHeight
         disableSelectionOnClick
+        autoHeight
       />
     </div>
   );
