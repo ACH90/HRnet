c3b596473c0ede548da339cb291c785f83352c93
ACH90 <chassagne.alexandre@gmail.com>
2025-05-28 13:49:24 +0200

    Refactor: début de migration vers Radix UI

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import styles from "./CreateEmployee.module.css";
 
 //Import reducers
 import { addEmployee } from "../../store/employeeSlice";
 
-//Import components
-// import Modal from "../../components/modal/modal";
 //-------------
 import Modal from "@ach90/hrnet-modal";
 import "@ach90/hrnet-modal/style.css";
@@ -69,95 +68,99 @@ export default function CreateEmployee() {
 
   return (
     <main>
-      <div className="title">
-        <h1>HRnet</h1>
-      </div>
-      <div className="container">
+      <header>
+        <div className="title">
+          <h1>HRnet</h1>
+        </div>
         <Link to="/employee-list">View Current Employees</Link>
         <h2>Create Employee</h2>
-        <form id="create-employee" onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="firstName">First Name</label>
-          <input
-            type="text"
-            id="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-          />
+      </header>
+      <div className={styles.container}>
+        <div className={styles.formContainer}>
+          <form id="create-employee" onSubmit={(e) => e.preventDefault()}>
+            <div className={styles.leftSide}>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
 
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            type="text"
-            id="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
 
-          <label>Date of Birth</label>
-          <DatePicker
-            value={formData.dateOfBirth}
-            onChange={(newValue) =>
-              setFormData({ ...formData, dateOfBirth: newValue })
-            }
-            format="DD/MM/YYYY"
-            slotProps={{ textField: { fullWidth: true } }}
-          />
+              <label>Date of Birth</label>
+              <DatePicker
+                value={formData.dateOfBirth}
+                onChange={(newValue) =>
+                  setFormData({ ...formData, dateOfBirth: newValue })
+                }
+                format="DD/MM/YYYY"
+                slotProps={{ textField: { fullWidth: true } }}
+              />
 
-          <label>Start Date</label>
-          <DatePicker
-            value={formData.startDate}
-            onChange={(newValue) =>
-              setFormData({ ...formData, startDate: newValue })
-            }
-            format="DD/MM/YYYY"
-            slotProps={{ textField: { fullWidth: true } }}
-          />
+              <label>Start Date</label>
+              <DatePicker
+                value={formData.startDate}
+                onChange={(newValue) =>
+                  setFormData({ ...formData, startDate: newValue })
+                }
+                format="DD/MM/YYYY"
+                slotProps={{ textField: { fullWidth: true } }}
+              />
+              <label htmlFor="department">Department</label>
+              <Dropdown
+                id="department"
+                value={formData.department}
+                onChange={handleDropdownChange}
+                options={departments}
+              />
+            </div>
+            <div className={styles.rightSide}>
+              <fieldset className="address">
+                <legend>Address</legend>
 
-          <fieldset className="address">
-            <legend>Address</legend>
+                <label htmlFor="street">Street</label>
+                <input
+                  id="street"
+                  type="text"
+                  value={formData.street}
+                  onChange={handleChange}
+                />
 
-            <label htmlFor="street">Street</label>
-            <input
-              id="street"
-              type="text"
-              value={formData.street}
-              onChange={handleChange}
-            />
+                <label htmlFor="city">City</label>
+                <input
+                  id="city"
+                  type="text"
+                  value={formData.city}
+                  onChange={handleChange}
+                />
 
-            <label htmlFor="city">City</label>
-            <input
-              id="city"
-              type="text"
-              value={formData.city}
-              onChange={handleChange}
-            />
+                <label htmlFor="state">State</label>
+                <Dropdown
+                  id="state"
+                  value={formData.state}
+                  onChange={handleDropdownChange}
+                  options={usStates}
+                />
 
-            <label htmlFor="state">State</label>
-            <Dropdown
-              id="state"
-              // label="State"
-              value={formData.state}
-              onChange={handleDropdownChange}
-              options={usStates}
-            />
-
-            <label htmlFor="zipCode">Zip Code</label>
-            <input
-              id="zipCode"
-              type="number"
-              value={formData.zipCode}
-              onChange={handleChange}
-            />
-          </fieldset>
-
-          <label htmlFor="department">Department</label>
-          <Dropdown
-            id="department"
-            // label="Department"
-            value={formData.department}
-            onChange={handleDropdownChange}
-            options={departments}
-          />
-        </form>
+                <label htmlFor="zipCode">Zip Code</label>
+                <input
+                  id="zipCode"
+                  type="number"
+                  value={formData.zipCode}
+                  onChange={handleChange}
+                />
+              </fieldset>
+            </div>
+          </form>
+        </div>
 
         <button onClick={handleSave}>Save</button>
       </div>
